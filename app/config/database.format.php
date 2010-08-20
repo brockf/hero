8863e9246aedf690d62addaaa46b4f3fbf4a1fc3
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-08-20 07:33:56 +0000

    Config fix.

--- app/config/database.format.php
+++ app/config/database.format.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = '';
 $db['default']['database'] = "opengateway";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
