ee8c51849f456fed2735ee6b584b5450c8766a1a
billy <billy@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-09-10 22:14:27 +0000

    Added events module folder

---
+++ app/modules/events/models/event_registration_model.php
@@ -0,0 +1,22 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Events Registration Model 
+*
+* Contains all the methods used to register events.
+*
+* @author Electric Function, Inc.
+* @copyright Electric Function, Inc.
+* @package Electric Publisher
+*
+*/
+
+class Event_registration_model.php extends CI_Model
+{
+	function __construct()
+	{
+		parent::CI_Model();
+	}
+	
+
+}
---
+++ app/modules/events/models/events_model.php
@@ -0,0 +1,22 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Events Model 
+*
+* Contains all the methods used to create, update, and delete events.
+*
+* @author Electric Function, Inc.
+* @copyright Electric Function, Inc.
+* @package Electric Publisher
+*
+*/
+
+class Events_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::CI_Model();
+	}
+	
+
+}
