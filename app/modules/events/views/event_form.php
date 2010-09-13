9f62f059c180e2a19b3f49f7eb9fd922ee423e25
billy <billy@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-09-13 14:38:29 +0000

    Events Form Changes

--- app/modules/events/views/event_form.php
+++ app/modules/events/views/event_form.php
@@ -6,12 +6,10 @@ if (!isset($event)) {
 				'title' => '',
 				'url_path' => '',
 				'description' => '',
-				'type' => FALSE,
-				'filter_authors' => array(0),
-				'filter_topics' => array(0),
-				'summary_field' => '',
-				'sort_field' => '',
-				'sort_dir' => 'ASC'
+				'location' => '',
+				'max_attendees' => '',
+				'price' => '',
+				'subscription' => ''
 			);
 }
 
@@ -44,57 +42,40 @@ if (!isset($event)) {
 	</ul>
 </fieldset>
 <fieldset>
-	<legend>Filters</legend>
-	<ul class="form">
-		<li>
-			<label for="type">Content Type</label>
-			<?=form_dropdown('type',$types,$event['type'],'id="content_type"');?>
-		</li>
-		<li>
-			<label for="authors">Author(s)</label>
-			<?=form_multiselect('authors[]',$users,$event['filter_authors']);?>
-		</li>
-		<li>
-			<label for="topics">Topic(s)</label>
-			<?=form_multiselect('topics[]',$topics,$event['filter_topics']);?>
-		</li>
-	</ul>
-</fieldset>
-<fieldset>
 	<legend>Options</legend>
 	<ul class="form">
 		<li>
-			<label for="summary_field">Summary Field</label>
-			<? if (isset($field_options)) { ?>
-				<? // we are editing and must have a field_options array which we can select from ?>
-				<?=form_dropdown('summary_field',$field_options,$event['summary_field'],'id="summary_field" class="editing populate_fields"');?>
-			<? } else { ?>
-				<?=form_dropdown('summary_field',array('' => 'Loading...'),'','id="summary_field" class="populate_fields"');?>
-			<? } ?>
+			<label class="full" for="location">Location</label>
 		</li>
 		<li>
-			<div class="help">This field populates each RSS feed item's summary.  It helps give users an idea of what the content is about.  All text will automatically be shortened to an appropriate length.</div>
+			<input type="text" class="required full text" id="location" name="location" value="<?=$event['location'];?>" />
+		</li>
+		<li>
+			<label class="full" for="max_attendees">Maximum Attendees</label>
+		</li>
+		<li>
+			<input type="text" class="required full text" id="max_attendees" name="max_attendees" value="<?=$event['max_attendees'];?>" />
+		</li>		
+		<li>
+			<label class="full" for="price">Price</label>
+		</li>
+		<li>
+			<input type="text" class="number text" id="price" name="price" value="<?=$event['price'];?>" />
 		</li>
 		
 		<li>
-			<label for="sort_field">Sort by</label>
-			<? if (isset($field_options)) { ?>
-			<? reset($field_options); ?>
-				<? // we are editing and must have a field_options array which we can select from ?>
-				<?=form_dropdown('sort_field',$field_options,$event['sort_field'],'id="sort_field" class="populate_fields editing"');?>
-			<? } else { ?>
-				<?=form_dropdown('sort_field',array('' => 'Loading...'),'','id="sort_field" class="populate_fields"');?>
-			<? } ?>
-			&nbsp;&nbsp;
-			<?=form_dropdown('sort_dir',array('ASC' => 'Ascending (First to Last, Oldest to Newest)', 'DESC' => 'Descending (Last to First, Newest to Oldest'), $event['sort_dir']);?>
+			<label class="full" for="v">Subscription</label>
 		</li>
+		<li>
+			<input type="text" class="number text" id="subscription" name="subscription" value="<?=$event['subscription'];?>" />
+		</li>			
 	</ul>
 </fieldset>
 
 <?=$form;?>
 
 <div class="submit">
-	<input type="submit" class="button" name="form_rss" value="Save RSS Feed" />
+	<input type="submit" class="button" name="form_event" value="Save Event" />
 </div>
 </form>
 <?=$this->load->view(branded_view('cp/footer'));?>
