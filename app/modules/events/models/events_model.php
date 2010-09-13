40b92419741b689e2a24cdd412afb86c5ea9d719
billy <billy@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-09-13 21:30:34 +0000

    event edit form changes

--- app/modules/events/models/events_model.php
+++ app/modules/events/models/events_model.php
@@ -20,6 +20,7 @@ class Events_model extends CI_Model
 	
 	/*
 	* Create New Event
+	*
  	* @param string $title Event title
  	* @param string $url_path
  	* @param string $description Event description
@@ -58,6 +59,68 @@ class Events_model extends CI_Model
 	}
 
 	/*
+	* Update RSS Feed
+	*
+	* @param string $event_id Event ID
+ 	* @param string $title Event title
+ 	* @param string $url_path
+ 	* @param string $description Event description
+ 	* @param string $location Event location
+ 	* @param string $max_attendees Event maximum attendees 
+ 	* @param string $price Event price 
+ 	* @param string $start_date Event start date
+ 	* @param string $end_date Event end date
+	* @param string Standard privileges array of member group ID's
+ 	*
+ 	* @return TRUE
+ 	*/
+	function update_event ($event_id, $title, $url_path, $description, $location, $max_attendees, $price, $start_date, $end_date, $privileges = array()) {
+		$event = $this->get_event($event_id);
+		
+		$this->load->model('link_model');
+		if ($url_path != $event['url_path']) {
+			$this->load->helper('clean_string');
+			$url_path = clean_string($url_path);
+			
+			$url_path = $this->link_model->get_unique_url_path($url_path);
+			$this->link_model->update_url($event['link_id'], $url_path);
+		}
+		$this->link_model->update_title($event['link_id'], $title);
+	
+		$update_fields = array(
+							'event_title' => $title,
+							'event_description' => $description,
+							'event_location' => $location,
+							'event_max_attendees' => $max_attendees,
+							'event_price' => $price,
+							'event_start_date' => $start_date,
+							'event_end_date' => $end_date,
+							'event_privileges' => (is_array($privileges) and !in_array(0, $privileges)) ? serialize($privileges) : ''
+							);
+							
+		$this->db->update('events',$update_fields,array('event_id' => $event_id));
+		
+		return TRUE;
+	}
+		
+	/*
+	* Get RSS Feed
+	*
+	* @param int $feed_id
+	*
+	* @return array
+	*/
+	function get_event ($event_id) {
+		$event = $this->get_events(array('id' => $event_id));
+		
+		if (empty($event)) {
+			return FALSE;
+		}
+		
+		return $event[0];
+	}
+	
+	/*
 	* Get Events
 	* @param int $filters['id']
 	* @param string $filters['title']
@@ -85,6 +148,7 @@ class Events_model extends CI_Model
 						'id' => $row['event_id'],
 						'link_id' => $row['link_id'],
 						'title' => $row['event_title'],
+						'url_path' => $row['event_url_path'],
 						'description' => $row['event_description'],
 						'location' => $row['event_location'],
 						'max_attendees' => $row['event_max_attendees'],
