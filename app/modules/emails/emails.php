d01481ae9dba8de5a25391d8cd744799c07bc61e
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-10-03 22:10:47 +0000

    Initial hooks commit.

--- app/modules/emails/emails.php
+++ app/modules/emails/emails.php
@@ -12,7 +12,7 @@
 */
 
 class Emails extends Module {
-	var $version = '1.0';
+	var $version = '1.01';
 	var $name = 'emails';
 
 	function __construct () {
@@ -30,5 +30,29 @@ class Emails extends Module {
 	function admin_preload ()
 	{
 		$this->CI->navigation->child_link('configuration',20,'Emails',site_url('admincp/emails'));
+	}
+	
+	function update ($db_version) {
+		if ($db_version < 1.01) {
+			$this->CI->settings_model->make_writeable_folder(setting('path_email_templates'), TRUE);
+			
+			$this->CI->db->query('DROP TABLE IF EXISTS `emails`');
+			$this->CI->db->query('DROP TABLE IF EXISTS `email_triggers`');
+			
+			$this->CI->db->query('CREATE TABLE `emails` (
+								  `email_id` int(11) NOT NULL auto_increment,
+								  `hook_name` varchar(125) NOT NULL,
+								  `email_parameters` TEXT NOT NULL,
+								  `email_subject_template` varchar(255) NOT NULL,
+								  `email_body_template` varchar(255) NOT NULL,
+								  `email_recipients` TEXT NOT NULL,
+								  `email_bccs` TEXT NOT NULL,
+								  `email_is_html` tinyint(1) NOT NULL,
+								  `email_deleted` tinyint(0) NOT NULL,
+								  PRIMARY KEY  (`email_id`)
+								) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;');
+		}
+		
+		return $this->version;
 	}
 }
