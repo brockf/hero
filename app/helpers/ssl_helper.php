abc5cabd1e5a700a1922c5695e172202ac2ce0bb
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-10-07 12:22:39 +0000

    Customize secure modules for SSL helper.

--- app/helpers/ssl_helper.php
+++ app/helpers/ssl_helper.php
@@ -11,11 +11,11 @@ function unsecure($url) {
 }
 
 if ($CI->config->item('ssl_certificate') == '1') {
-	if ((in_array($CI->uri->segment(1),$config['secure_modules'])) and ($_SERVER["SERVER_PORT"] != "443" or (isset($_SERVER['HTTPS']) and $_SERVER['HTTPS'] != 'on'))) {
+	if ((in_array($CI->uri->segment(1),$CI->config->item('secure_modules'))) and ($_SERVER["SERVER_PORT"] != "443" or (isset($_SERVER['HTTPS']) and $_SERVER['HTTPS'] != 'on'))) {
 		header("Location: " . secure(current_url()));
 		die();
 	}
-	elseif (($_SERVER["SERVER_PORT"] == "443" or (isset($_SERVER['https']) and $_SERVER['HTTPS'] == 'on')) and (!in_array($config['secure_modules'], $CI->uri->segment(1)))) {
+	elseif (($_SERVER["SERVER_PORT"] == "443" or (isset($_SERVER['https']) and $_SERVER['HTTPS'] == 'on')) and (!in_array($CI->uri->segment(1),$CI->config->item('secure_modules')))) {
 		header('Location: ' . unsecure(current_url()));
 		die();
 	}		
