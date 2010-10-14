8ba5adc6a75ab605104fd55b0c7cf1029d47d86e
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2010-10-14 05:03:13 +0000

    Product options completely integrated in backend and invoices!

--- app/modules/reports/views/products.php
+++ app/modules/reports/views/products.php
@@ -7,7 +7,18 @@ if (!empty($this->dataset->data)) {
 	?>
 		<tr <? if (isset($row['shipped']) and empty($row['shipped'])) { ?>class="highlight"<? } ?>>
 			<td><?=$row['invoice_id'];?></td>
-			<td><?=$row['name'];?></td>
+			<td>
+				<?=$row['name'];?>
+				
+				<? if (!empty($row['options'])) { ?>
+					<span class="product_options">
+					<? foreach ($row['options'] as $label => $value) { ?>
+						<?=$label;?>: <?=$value;?>&nbsp;&nbsp;
+					<? } ?>
+					</span>
+				<? } ?>
+			
+			</td>
 			<td><?=$row['quantity'];?></td>
 			<td><?=$row['date'];?></td>
 			<td><input type="radio" class="shipped_no" rel="<?=$row['order_products_id'];?>" name="shipped_<?=$row['order_products_id'];?>" <? if ($row['shipped'] == FALSE) { ?>checked="checked"<? } ?>value="0" /> No  <input type="radio" class="shipped_yes" rel="<?=$row['order_products_id'];?>" <? if ($row['shipped'] == TRUE) { ?>checked="checked"<? } ?> name="shipped_<?=$row['order_products_id'];?>" value="1" /> Yes</td>
