d5d617a3826e8a89330d75e032f07c359f575ce7
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-02-10 04:49:31 +0000

    Cube theme.

--- themes/orchard/js/universal.js
+++ themes/orchard/js/universal.js
@@ -1,33 +1,19 @@
 $(document).ready(function () {
 	// drop down menu
-	$('div#navigation ul a').click(function () {
-		var children = $(this).parent().children('ul.children');
+	$('div#navigation ul li').hover(function () {
+		var children = $(this).children('ul.children');
 		
 		if (children.length > 0) {
-			if (children.filter(':visible').length > 0) {
-				// menu already showing, pass the user on
-				return true;
-			}
-			else {
-				// menu not showing, drop it down
-				
-				// close others
-				$('div#navigation ul.children').slideUp('fast');
-				
-				children.slideDown('fast');
-				return false;
-			}
+			children.stop(true,true).slideDown('fast');
 		}
-		else {	
-		 	// no sub-menu, pass them on
-			return true;
+	}, function () { 
+		var children = $(this).children('ul.children');
+		
+		if (children.length > 0) {
+			children.delay(1).stop(true,true).slideUp('fast');
 		}
 	});
 	
-	$(document).click(function () {
-		$('div#navigation ul.children').slideUp('fast');
-	});
-
 	// submit button
 	$('input.button').hover(function () {
 		$(this).toggleClass('hover');
