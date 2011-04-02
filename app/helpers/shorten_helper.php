27efe03ded30b73c5c967163d77899ca24cefe9a
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-04-02 16:50:06 +0000

    Added {subscription_plans}

--- app/helpers/shorten_helper.php
+++ app/helpers/shorten_helper.php
@@ -6,11 +6,11 @@ function shorten ($string, $length, $retain_whole_words = FALSE) {
 	if (strlen(strip_tags($string)) > $length) {
 		if ($retain_whole_words == FALSE) {
 			$string = substr($string, 0, ($length - 3));
-			$string .= '&hellip';
+			$string .= '&hellip;';
 		}
 		else {
 			$string = preg_replace('/\s+?(\S+)?$/', '', substr($string, 0, ($length - 3)));
-			$string .= '&hellip';
+			$string .= '&hellip;';
 		}
 		
 		return $string;
