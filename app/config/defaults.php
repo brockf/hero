361bb5b99663f057acef5f34bb04f4a04879d422
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-04-19 20:01:18 +0000

    Tweaking to use default GET string support.

---
+++ app/config/defaults.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* Default Configurations
+*
+* These configurations were added post-release.  They are specified here because they are
+* default and very unlikely to be changed at the user's site.
+*/
+
+$config['allow_get_array'] = TRUE;
