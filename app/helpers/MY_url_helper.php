bb8a14ec0fc4130c7ac0aa0e892d3abcf5b8bea8
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-04-20 04:34:43 +0000

    Remove trailing ? from url in current_url().

--- app/helpers/MY_url_helper.php
+++ app/helpers/MY_url_helper.php
@@ -7,5 +7,8 @@ function current_url () {
 	$CI =& get_instance();
 	$base_url = $CI->config->site_url($CI->uri->uri_string()) . '?' . http_build_query($_GET);
 	
+	// remove trailing question mark if there is one
+	$base_url = rtrim($base_url, '?');
+	
 	return $base_url;
 }
