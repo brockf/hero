960e26a483e1e59dd532806355d50da636ead018
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-05-01 22:03:53 +0000

    Fixed improper timestamp reference for signupdate viewing in members list.

--- app/modules/users/controllers/admincp.php
+++ app/modules/users/controllers/admincp.php
@@ -132,7 +132,7 @@ class Admincp extends Admincp_Controller {
 		$options['email'] = array('name' => 'Email', 'type' => 'text', 'filter' => 'email', 'sort_column' => 'user_email');
 		$options['groups'] = array('name' => 'Groups', 'type' => 'select', 'options' => $usergroups, 'filter' => 'group');
 		$options['status'] = array('name' => 'Status', 'type' => 'select', 'filter' => 'suspended', 'options' => array('0' => 'Active', '1' => 'Suspended'));
-		$options['signup_date'] = array('name' => 'Signup Date', 'type' => 'date', 'sort_column' => 'user_signup_date', 'filter' => 'timestamp', 'field_start_date' => 'signup_start_date', 'field_end_date' => 'signup_end_date');
+		$options['signup_date'] = array('name' => 'Signup Date', 'type' => 'date', 'sort_column' => 'user_signup_date', 'filter' => 'signup_date', 'field_start_date' => 'signup_start_date', 'field_end_date' => 'signup_end_date');
 		
 		// custom fields
 		$custom_fields = $this->custom_fields_model->get_custom_fields(array('group' => '1'));
