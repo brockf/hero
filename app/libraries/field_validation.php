a6a9a038131a5f62d939e334838e3a8200b10bfb
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-06-01 20:38:26 +0000

    Show reduced price for members prior to adding to cart.

--- app/libraries/field_validation.php
+++ app/libraries/field_validation.php
@@ -39,9 +39,14 @@ class Field_validation
 		}
 	}
 	
-	function ValidateEmailAddress($email)
+	function ValidateEmailAddress ($email)
 	{
-		return preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $email);
+		if (strpos($email, '.') !== FALSE and strpos($email, '@') !== FALSE) {
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
 	}
 	
 	/**
