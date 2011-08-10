9bdf9b8aa7317575347936cb912702ba9a49647f
brockf <brockf@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2011-08-10 17:07:42 +0000

    Custom field fix (max 50 characters on field names).

--- app/models/custom_fields_model.php
+++ app/models/custom_fields_model.php
@@ -149,8 +149,8 @@ class Custom_fields_model extends CI_Model {
 		$this->load->helper('clean_string');
 		$system_name = clean_string($name);
 		
-		if (strlen($system_name) > 64) {
-			$system_name = substr($system_name, 0, 64);
+		if (strlen($system_name) > 50) {
+			$system_name = substr($system_name, 0, 50);
 		}
 		
 		// get next order
@@ -245,6 +245,10 @@ class Custom_fields_model extends CI_Model {
 		// calculate system name
 		$this->load->helper('clean_string');
 		$system_name = clean_string($name);
+		
+		if (strlen($system_name) > 50) {
+			$system_name = substr($system_name, 0, 50);
+		}
 		
 		$update_fields = array(
 							'custom_field_group' => $group,
