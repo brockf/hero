b1631e97e9deda8ef904a757bded1fa1858ea420
Lonnie <Lonnie@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2013-01-11 14:16:57 +0000

    Small bug fix to the unused head_compile library. Thanks Dam!

--- app/libraries/head_compile.php
+++ app/libraries/head_compile.php
@@ -13,25 +13,25 @@
 
 class Head_compile {
 	var $CI;
-	
+
 	function __construct () {
 		$this->CI =& get_instance();
 	}
-	
+
 	function compile () {
 		$output = $this->CI->output->get_output();
-		
+
 		// get <head> contents
 		$matches = array();
 		preg_match('#<head>(.*?)<\/head>#si',$output,$matches);
-		
+
 		// head contents
 		$head = $matches[1];
-		
+
 		// get JS includes via <script> references
 		preg_match_all('#<script(.*?)src="(.*?)"(.*?)>(.*?)<\/script>#si',$head,$matches);
 		$js_includes = $matches[2];
-		
+
 		// delete non-local includes
 		foreach ($js_includes as $key => $script) {
 			if (strpos($script,'//') !== FALSE and strpos($script,$this->CI->config->item('base_url')) === FALSE) {
@@ -39,20 +39,20 @@ class Head_compile {
 				unset($js_includes[$key]);
 			}
 		}
-		
+
 		if (!empty($js_includes)) {
 			$this->load->library('JSMin');
-			
+
 			// minify!
 			$js_compiled = '';
 			foreach ($js_includes as $script) {
 				// get the file
-				$js_compiled .= JSMin::minify($)
+				$js_compiled .= JSMin::minify($script);
 			}
 		}
-		
+
 		// TODO:
-		
+
 		// - load the files for minification
 		//		- this includes using the base_href tag if one exists, otherwise loading paths relative to the domain or
 		//		  using the full script URL if it's in that format
