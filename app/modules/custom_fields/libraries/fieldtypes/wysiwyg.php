abba26048058943499135a88ad70aaa3cc2e5eba
Lonnie <Lonnie@979dc0ca-a40d-44cd-afcf-df9d93f6ecad>
2013-03-14 13:45:07 +0000

    Whitespace cleanup.

--- app/modules/custom_fields/libraries/fieldtypes/wysiwyg.php
+++ app/modules/custom_fields/libraries/fieldtypes/wysiwyg.php
@@ -16,7 +16,7 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	*/
 	function __construct () {
 		parent::__construct();
-	 
+
 		$this->compatibility = array('publish','products','collections');
 		$this->enabled = TRUE;
 		$this->fieldtype_name = 'WYSIWYG Textarea';
@@ -24,7 +24,7 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 		$this->validation_error = '';
 		$this->db_column = 'TEXT';
 	}
-	
+
 	/**
 	* Set Height
 	*
@@ -37,10 +37,10 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	*/
 	function height ($height) {
 		$this->data['height'] = $height;
-		
+
 		return $this;
 	}
-	
+
 	/**
 	* Output Shared
 	*
@@ -54,24 +54,24 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 		if ($this->width == FALSE) {
 			$this->width = '750px';
 		}
-		
+
 		if (!isset($this->data['height']) or $this->data['height'] == FALSE) {
 			$this->data['height'] = '140px';
 		}
-		
+
 		// prep classes
 		if ($this->required == TRUE) {
 			$this->field_class('required');
 		}
-		
+
 		$this->field_class('wysiwyg');
-		
+
 		// add validator names to class list
 		foreach ($this->validators as $validator) {
 			$this->field_class($validator);
 		}
-		
-		// prep final attributes	
+
+		// prep final attributes
 		$attributes = array(
 						'type' => 'textarea',
 						'name' => $this->name,
@@ -79,13 +79,13 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 						'id' => $this->name,
 						'class' => implode(' ', $this->field_classes)
 						);
-		
+
 		// compile attributes
 		$attributes = $this->compile_attributes($attributes);
-		
+
 		return $attributes;
 	}
-	
+
 	/**
 	* Output Admin
 	*
@@ -97,23 +97,23 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 		if (empty($this->value) and $this->CI->input->post($this->name) == FALSE) {
 			$this->value($this->default);
 		}
-		
+
 		if (!in_array('complete',$this->field_classes) or (isset($this->data['use_basic']) and $this->data['use_basic'] == TRUE)) {
 			$this->field_class('basic');
 		}
 		else {
 			$this->field_class('complete');
 		}
-	
+
 		$attributes = $this->output_shared();
-		
+
 		// get CKEditor to show
 		if (!defined('INCLUDE_CKEDITOR')) {
 			define('INCLUDE_CKEDITOR','TRUE');
 		}
-		
+
 		$help = ($this->help == FALSE) ? '' : '<div class="help" style="margin-left: 0">' . $this->help . '</div>';
-		
+
 		// build HTML
 		$return = '<li>
 						<label for="' . $this->name . '">' . $this->label . '</label>
@@ -122,10 +122,10 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 						' . $help . '
 						</div>
 					</li>';
-					
+
 		return $return;
 	}
-	
+
 	/**
 	* Output Frontend
 	*
@@ -136,7 +136,7 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	function output_frontend () {
 		return FALSE;
 	}
-	
+
 	/**
 	* Validation Rules
 	*
@@ -147,7 +147,7 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	*/
 	function validation_rules () {
 		$rules = array();
-		
+
 		// run $this->validators
 		if (!empty($this->validators)) {
 			foreach ($this->validators as $validator) {
@@ -156,15 +156,15 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 				}
 			}
 		}
-		
+
 		// check required
 		if ($this->required == TRUE) {
 			$rules[] = 'required';
 		}
-		
+
 		return $rules;
 	}
-	
+
 	/**
 	* Validate Post
 	*
@@ -181,7 +181,7 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 		// nothing extra to validate here other than the rulers in $this->validators
 		return TRUE;
 	}
-	
+
 	/**
 	* Post to Value
 	*
@@ -192,42 +192,42 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	function post_to_value () {
 		return $this->CI->input->post($this->name);
 	}
-	
+
 	/**
 	* Field Form
 	*
 	* Build the form that will be used to add/edit fields of this type.
-	* 
+	*
 	* @return string $form Built using form_builder.
 	*/
 	function field_form ($edit_id = FALSE) {
 		// build fieldset with admin_form which is used when editing a field of this type
 		$this->CI->load->library('custom_fields/form_builder');
 		$this->CI->form_builder->reset();
-		
+
 		$default = $this->CI->form_builder->add_field('textarea');
 		$default->label('Default Value')
 	          ->name('default')
 	          ->width('500px');
-	          
+
 	    $help = $this->CI->form_builder->add_field('textarea');
 	    $help->label('Help Text')
 	    	 ->name('help')
 	    	 ->width('500px')
 	    	 ->height('80px')
 	    	 ->help('This help text will be displayed beneath the field.  Use it to guide the user in responding correctly.');
-	    	 
+
 	    $required = $this->CI->form_builder->add_field('checkbox');
 	    $required->label('Required Field')
 	    	  ->name('required')
 	    	  ->help('If checked, this box must not be empty for the form to be processed.');
-	    	  
+
 	   	$basic = $this->CI->form_builder->add_field('checkbox');
 	   	$basic->label('Use Basic Editor')
 	   		  ->name('use_basic')
 	   		  ->help('The "Basic" editor doesn\'t have all of the features of the WYSIWYG editor, but is more appropriate when you just want
 	   		          basic HTML stylings, images, links, etc.');
-	    	  
+
 	   	$validators = $this->CI->form_builder->add_field('multicheckbox');
 	   	$validators->label('Validators')
 	   			   ->name('validators')
@@ -236,39 +236,39 @@ class Wysiwyg_Fieldtype extends Fieldtype {
 	   			   			array('name' => 'Trim whitespace from around response', 'value' => 'trim')
 	   			   		)
 	   			   );
-	    	  
+
 	    if (!empty($edit_id)) {
 	    	$this->CI->load->model('custom_fields_model');
 	    	$field = $this->CI->custom_fields_model->get_custom_field($edit_id);
-	    	
+
 	    	$default->value($field['default']);
 	    	$help->value($field['help']);
 	    	$validators->value($field['validators']);
 	    	$required->value($field['required']);
-	    	
+
 	    	if (isset($field['data']['use_basic']) and $field['data']['use_basic'] == TRUE) {
 	    		$basic->value(TRUE);
 	    	}
-	    }	  
-	          
-		return $this->CI->form_builder->output_admin();      
+	    }
+
+		return $this->CI->form_builder->output_admin();
 	}
-	
+
 	/**
 	* Field Form Process
 	*
 	* Process the submission of $this->field_form() and return an array of data to be used in custom_fields_model->new_custom_field().
 	*
-	* Available keys for the returned array: name, type, default (string/array), help, required, validators (array), data (array), 
+	* Available keys for the returned array: name, type, default (string/array), help, required, validators (array), data (array),
 	*										 options (array), width
 	*
 	* @return array
 	*/
 	function field_form_process () {
 		// build array for database
-		
+
 		// $options will be automatically serialized by the custom_fields_model::new_custom_field() method
-		
+
 		return array(
 					'name' => $this->CI->input->post('name'),
 					'type' => $this->CI->input->post('type'),
