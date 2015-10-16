713fb1f856ff47e6e496372909e4b5f0b09fb95c
josev814 <josev814@gmail.com>
2015-10-16 16:31:45 -0400

    Modified the Twitter file to create a table for all of the tweets sent, and added options for bit.ly login and user_key

--- app/modules/twitter/twitter.php
+++ app/modules/twitter/twitter.php
@@ -12,7 +12,7 @@
 */
 
 class Twitter_module extends Module {
-	public $version = '1.01';
+	public $version = '1.10';
 	public $name = 'twitter';
 
 	function __construct () {
@@ -40,7 +40,23 @@ class Twitter_module extends Module {
 	* @return int The current software version, to update the database
 	*/
 	function update ($db_version) {
-		if ($db_version < '1.0') {
+		if ($db_version < '1.01') {
+			$this->CI->db->query('CREATE TABLE `tweets_sent` (
+ 								 `tweet_id` int(11) NOT NULL auto_increment,
+ 								 `tweet` varchar(255) NOT NULL,
+  								 `sent_time` DATETIME,
+  								 `content_id` int(11) NOT NULL,
+ 								 `content_type` int(11) NOT NULL,
+ 								 PRIMARY KEY  (`tweet_id`)
+								 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;');
+		}
+		if($db_version < '1.01'){
+			// initial install
+			$this->CI->settings_model->new_setting(1, 'bitly_id', '', '', 'text','', FALSE, FALSE);
+			$this->CI->settings_model->new_setting(1, 'bitly_api_key', '', '', 'text','', FALSE, FALSE);
+		}
+		if ($db_version < '1.01') {
+			
 			// initial install
 			$this->CI->settings_model->new_setting(1, 'twitter_consumer_key', '', '', 'text','', FALSE, TRUE);
 			$this->CI->settings_model->new_setting(1, 'twitter_consumer_secret', '', '', 'text','', FALSE, TRUE);
